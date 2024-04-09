7d5ad9062666129c7b788e5d212074d06a3b11f7
Ficussag <see1sawkuwraw@gmail.com>
2024-04-09 20:00:52 +0300

    changed the appearance of the snake

--- zmeika.cpp
+++ zmeika.cpp
@@ -47,7 +47,7 @@ void Draw()
                 {
                     if (tailX[k] == j && tailY[k] == i)
                     {
-                        cout << "*"; print = true;
+                        cout << "@"; print = true;
                     }
                 }
                 if (!print) cout << " ";
