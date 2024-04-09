c46c44dd080c233b7b1b2f75135fa154c6abb982
Ficussag <see1sawkuwraw@gmail.com>
2024-04-09 19:26:08 +0300

    Wrote code as assigned /*  Remove even numbers from the created list. */

--- pidor.cpp
+++ pidor.cpp
@@ -1,5 +1,34 @@
 /*
-* Удалить из созданного списка четные числа.
-* сын шлюхи
+ Удалить из созданного списка четные числа.
 */
 #include <iostream>
+
+using namespace std;
+
+int main() {
+	// создал массив
+	int numbers[] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+	int size = sizeof(numbers) / sizeof(numbers[0]); // Определяем размер массива
+
+   
+    int* oddNumbers = new int[size];
+    int oddCount = 0; // Счетчик для нечетных чисел
+
+    
+    for (int i = 0; i < size; ++i) {
+        if (numbers[i] % 2 != 0) {
+            oddNumbers[oddCount] = numbers[i];
+            ++oddCount;
+        }
+    }
+
+   
+    for (int i = 0; i < oddCount; ++i) {
+        std::cout << oddNumbers[i] << " ";
+    }
+
+    
+    delete[] oddNumbers;
+
+    return 0;
+}
